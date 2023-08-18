8d6a36e2266f8787bc573ae7edc4b642809d4706
Mohammed Ahmed Fulwala <mohammed.fulwala@hotmail.com>
2023-08-17 23:04:48 -0400

    added login and navbar

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -1,11 +1,68 @@
 import './App.css';
+import { Amplify } from 'aws-amplify';
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+import { Box, Grid, Typography } from '@mui/material'
+import { View, Image } from '@aws-amplify/ui-react';
+import { useTheme } from '@aws-amplify/ui-react';
+import awsExports from './aws-exports';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { NavBar } from './components/NavBar';
 
-function App() {
+Amplify.configure(awsExports);
+
+const appStyles = {
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  height: '100vh', // Adjust this as needed
+};
+
+
+const components = {
+  Header() {
+    const { tokens } = useTheme();
+    return (
+      <Box sx={{display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        <View padding={tokens.space.large}>
+          <Image
+            alt="Amplify logo"
+            src="https://docs.amplify.aws/assets/logo-dark.svg"
+          />
+        </View>
+        {/* <img src={logo} alt='logo' /> */}
+      </Box>
+    )
+  },
+
+  SignUp: {},
+
+  Footer() {
+    return (
+      <Grid item container xs={12} sx={{display:'flex', justifyContent:'center',alignItems:'center', height:'100%'}}>
+        <View>
+          <Typography align='center' variant='subtitle1' color='text.secondary'>Copyright VideoCo &copy; 2022 | All Rights Reserved</Typography>
+        </View>
+      </Grid>
+    )
+  }
+}
+
+
+export default function App() {
   return (
-    <div className="App">
-      <h1>Welcome to VideoCo</h1>
+    <div style={appStyles}>
+      <Authenticator components={components}>
+        {({ signOut, user }) => (
+          <BrowserRouter>
+          <NavBar>
+            {/* <Routes>
+
+            </Routes> */}
+          </NavBar>
+          </BrowserRouter>
+        )}
+      </Authenticator>
     </div>
   );
 }
-
-export default App;
